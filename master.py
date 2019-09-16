23e2f8fc882e510aa483001b3b04601d929a9c80
Dacastillo <dacastillo@uc.cl>
2019-09-16 17:38:12 -0300

    modificando

--- master.py
+++ master.py
@@ -2,9 +2,17 @@ from qutip import *
 from matplotlib.pyplot import *
 import numpy as np
 H = np.sqrt(1.1) * sigmap() - np.sqrt(0.1) * sigmam()
-psi0 = projection(2,0,1)
-times = np.linspace(int(0.0), int(10.0), int(200.0))
-result = mesolve(H, psi0, times, [], [])
+est0=qeye(2)
+est1=projection(2,0,0)
+est2=projection(2,1,1)
+times = np.linspace(int(0.0), int(10.0), int(60.0))
+result1 = mesolve(H, est0, times)
+result2 = mesolve(H, est1, times)
+result3 = mesolve(H, est2, times)
+fin0 = result1.states[2]
+fin1 = result2.states[2]
+fin2 = result3.states[2]
+eig1 = fin0.eigenstates[0]
 #figure()
 #plot(times, result.expect[0])
 #plot(times, result.expect[1])
