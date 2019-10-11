2ce1566a2ee308e45d5892e7caaf5fd558522b6b
Dacastillo <dacastillo@uc.cl>
2019-10-10 23:30:58 -0300

    Cambio generado con script

--- t2.py
+++ t2.py
@@ -1,8 +1,9 @@
 from matplotlib.pyplot import *
 import numpy as np
+from scipy.linalg import expm, sinm, cosm
 sigmas = np.array([[0,0,1,0],[0,0,0,1],[0,0,0,0],[0,0,0,0]])
 sigmen = sigmas.transpose()
-a = np.array([[0,1,0,0],[0,0,0,0],[0,0,0,1],[0,0,0,0]])
+a = np.array([[0,0,0,0],[1,0,0,0],[0,0,0,0],[0,0,1,0]])
 adag = a.transpose()
 Hjc = sigmas.dot(sigmen)+ adag.dot(a)+(adag.dot(sigmen)+a.dot(sigmas))/1000
 e1 = np.array([0,1,0,0])
@@ -11,17 +12,46 @@ S1 = np.sqrt(0.1)*sigmen
 S1d = S1.transpose()
 S2 = np.sqrt(0.2)*a
 S2d = S2.transpose()
-print(Hjc)
-print(S1)
-print(S2)
-mat = d1
-print('ecuacion maestra')
-for i in range (0,100):
-    mat = mat -0.000000000000000065*(0.05j*(Hjc.dot(d1)-d1.dot(Hjc)))
-    mat = mat +0.000000000000000065*(2*S1d.dot(S1.dot(d1))+2*d1.dot(S1d.dot(S1))-S1.dot(d1.dot(S1d)))
-    mat = mat +0.000000000000000065*(2*S2d.dot(S2.dot(d1))+2*d1.dot(S2d.dot(S2))-S2.dot(d1.dot(S2d)))
-    print(mat[0][0],mat[1][1],mat[2][2],mat[3][3]) 
-print('quantum jumps')
-a=np.random.random_sample()
-b=np.random.random_sample()
-print(a,b)
+ene=100
+emas=[]
+eqju=[]
+deltat=5/ene
+tiempo=[deltat*i for i in range (ene)]
+#ecuacion maestra
+for i in range (0,ene):
+    mat = d1
+    mat = mat -deltat*(1j*(Hjc.dot(mat)-mat.dot(Hjc)))
+    mat = mat +deltat*(S1.dot(mat.dot(S1d))-0.5*mat.dot(S1d.dot(S1))-0.5*S1d.dot(S1.dot(mat)))
+    mat = mat +deltat*(S2.dot(mat.dot(S2d))-0.5*mat.dot(S2d.dot(S2))-0.5*S2d.dot(S2.dot(mat)))
+    d1 = mat
+    emas.append(np.absolute(mat.dot(sigmen.dot(sigmas)).trace())) 
+#quantum jumps
+for i in range (0,ene):
+    vec = e1
+    rn1=np.random.random_sample()
+    rn2=np.random.random_sample()
+    deltap1=np.absolute(deltat*np.dot(S1.dot(vec),S1.dot(vec)))
+    deltap2=np.absolute(deltat*np.dot(S2.dot(vec),S2.dot(vec)))
+    def tongo(v):
+        norm = np.linalg.norm(v)
+        if norm == 0:
+           return v
+        else:
+           return v/norm    
+    if(deltap1+deltap2==0):
+       deltasum=0
+    else:
+       deltasum=deltap1+deltap2
+    if(rn1 <= deltap1+deltap2):
+        if(rn2 <= deltap1/deltasum):
+            vec=tongo(S1.dot(vec))
+        else:
+            vec=tongo(S2.dot(vec))   
+    else:
+        vec=tongo(expm(-1j*(Hjc-0.5j*(S1d.dot(S1)+S2d.dot(S2)))*deltat).dot(vec))
+    e1 = vec
+    eqju.append(np.absolute(np.dot(sigmas.dot(vec),sigmas.dot(vec))))
+
+plot(tiempo,emas)
+plot(tiempo,eqju)
+show()
