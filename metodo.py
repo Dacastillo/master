4fe8f41a286d83ea4b3564fa0aaba2fe44eb5478
Dacastillo <dacastillo@uc.cl>
2019-10-16 21:30:26 -0300

    Cambio generado con script

--- metodo.py
+++ metodo.py
@@ -1,6 +1,5 @@
 from matplotlib.pyplot import *
 import numpy as np
-from scipy.linalg import expm, sinm, cosm
 sigmas = np.array([[0,0,1,0],[0,0,0,1],[0,0,0,0],[0,0,0,0]])
 sigmen = sigmas.transpose()
 a = np.array([[0,0,0,0],[1,0,0,0],[0,0,0,0],[0,0,1,0]])
@@ -20,7 +19,7 @@ tiempo=[deltat*i for i in range (ene)]
 #ecuacion maestra
 for i in range (0,ene):
     mat = d1
-    mat = mat -deltat*(1j*(Hjc.dot(mat)-mat.dot(Hjc)))
+    mat = mat -deltat*(1j*(np.tensordot(Hjc,mat,0)-np.tensordot(mat,Hjc,0)))
     mat = mat +deltat*(S1.dot(mat.dot(S1d))-0.5*mat.dot(S1d.dot(S1))-0.5*S1d.dot(S1.dot(mat)))
     mat = mat +deltat*(S2.dot(mat.dot(S2d))-0.5*mat.dot(S2d.dot(S2))-0.5*S2d.dot(S2.dot(mat)))
     d1 = mat
@@ -30,28 +29,25 @@ for i in range (0,ene):
     vec = e1
     rn1=np.random.random_sample()
     rn2=np.random.random_sample()
-    deltap1=np.absolute(deltat*np.dot(S1.dot(vec),S1.dot(vec)))
-    deltap2=np.absolute(deltat*np.dot(S2.dot(vec),S2.dot(vec)))
+    deltap1=deltat*np.abs(np.dot(S1.dot(vec),S1.dot(vec)))
+    deltap2=deltat*np.abs(np.dot(S2.dot(vec),S2.dot(vec)))
     def tongo(v):
         norm = np.linalg.norm(v)
-        if norm == 0:
+        if (norm == 0):
            return v
         else:
            return v/norm    
-    if(deltap1+deltap2==0):
-       deltasum=0
-    else:
-       deltasum=deltap1+deltap2
-    if(rn1 <= deltap1+deltap2):
-        if(rn2 <= deltap1/deltasum):
-            vec=tongo(S1.dot(vec))
-        else:
+    print(rn1,rn2,deltap1,deltap2)
+    if(rn1<=0.5):
+       if(rn2<=0.2):
+           vec=tongo(S1.dot(vec))
+       else:
             vec=tongo(S2.dot(vec))   
     else:
-        vec=tongo(vec-1j*(Hjc-0.5j*(S1d.dot(S1)+S2d.dot(S2)))*deltat.dot(vec))
+       vec=tongo(vec-1j*deltat*(Hjc.dot(vec)-0.5j*(S1d.dot(S1)+S2d.dot(S2))))
     e1 = vec
     eqju.append(np.absolute(np.dot(sigmas.dot(vec),sigmas.dot(vec))))
-
-plot(tiempo,emas)
-plot(tiempo,eqju)
-show()
+#print(eqju)
+#plot(tiempo,emas)
+#plot(tiempo,eqju)
+#show()
